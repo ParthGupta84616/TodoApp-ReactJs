88c63756410f2965957c98cb8aedb915b7025f80
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-03-09 00:58:32 +0530

    Deletion of Todos done

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Todo from './components/Todo';
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [todos, setTodos] = useState([]);
+  const [inputDate, setInputDate] = useState(null)
 
   const handleClick = () => {
-
     if (inputValue.trim() !== '') {
-      setTodos([...todos , inputValue.trim()]);
+      setTodos([...todos , {id:new Date().toUTCString(),text:inputValue.trim(),deadline:inputDate}]);
       setInputValue('');
+      console.log(inputDate);
+      setInputDate(null)
     }
   };
   const handleKey = (event) => {
@@ -21,19 +23,41 @@ function App() {
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
-  
+  const handleDate = (event) => {
+    setInputDate(event.target.value);
+  };
+
+  const todoDelete = (id) =>{
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
 
   return (
-    <div className='bg-slate-800 w-screen h-screen flex justify-center'>
-      <div className="wrapper flex flex-col items-center mt-64  w-2/3">
-        <div className="box1 rounded-xl border-black border-2 flex h-16 w-2/3">
+    <div className='bg-slate-900 w-screen h-screen '>
+      <div className="wrapper flex flex-col items-center p-8 ">
+        <div className="box1 rounded-xl border-black border-2 flex h-16 mt-8 w-full  ">
           <input
-            className='bg-gray-400 rounded-xl text-blue-900 text-center text-3xl w-5/6'
+            className='bg-gray-400 rounded-xl text-blue-900 text-center text-3xl w-4/6'
             type="text"
             value={inputValue}
             onChange={handleChange}
             onKeyDown={handleKey}
           />
+          
+          <div className='w-1/6  flex text-green-600'>
+          <label htmlFor="datetime">
+            <button >Enter Deadline</button>
+          </label>
+          <input 
+          className=''
+            type="datetime-local" 
+            id="datetime" 
+            name="datetime"
+            value={inputDate}
+            onChange={handleDate}
+          />
+          </div>
+
+
           <button
             onClick={handleClick}
             className='rounded-xl bg-green-500 w-1/6'
@@ -41,10 +65,10 @@ function App() {
             Add
           </button>
         </div>
-        <div className="box2 text-white bg-slate-900 border-slate-800 border-4 mt-4 w-2/3">
+        <div className="box2 text-white bg-slate-900 border-slate-800 border-4 mt-4 w-full">
           {todos.map((todo, index) => (
             
-            <Todo todo={todo} />
+            <Todo todo={todo} id={todo.id} todoDelete={todoDelete}/>
           ))}
         </div>
       </div>
