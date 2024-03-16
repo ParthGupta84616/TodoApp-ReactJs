31c765f843915224d87aefc2f0c30832668402ba
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-03-16 21:18:47 +0530

    Final frontend completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,13 +19,19 @@ function Header() {
         </a>
     </div>
     
-    <div className='flex items-center justify-center m-8 -mb-8 text-5xl font-bold text-orange-700'>
-        <img src={Logo} alt="" 
-        className='size-24 m-8 rounded-full'
-        />
+    <div className="flex justify-center ">
+      <div className="flex items-center justify-center m-8 -mb-8 text-5xl font-bold text-orange-700 w-3/4">
+        <img src={Logo} alt="" className="w-24 h-24 m-8 rounded-full" />
         <h1>Todo App</h1>
-
+      </div>
+      <div className="flex items-center relative justify-end m-8 -mb-8 text-xl font-bold text-orange-700 w-1/4">
+        <input type="text" placeholder="Username" className="w-40 px-4 py-2 mr-4 rounded-lg border focus:outline-none focus:border-blue-500" />
+        <input type="password" placeholder="Password" className="w-40 px-4 py-2 mr-4 rounded-lg border focus:outline-none focus:border-blue-500" />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Submit</button>
+      </div>
     </div>
+
+
     
     </>
   )
